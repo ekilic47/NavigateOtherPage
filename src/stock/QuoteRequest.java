d61836a0ee2e7781bfd693c46d5b20cb13107c90
ekilic47 <efecankilic47@gmail.com>
2015-08-26 16:00:47 +0300

    Inıtial submit.

---
+++ src/stock/QuoteRequest.java
@@ -0,0 +1,40 @@
+package stock;
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+
+@ManagedBean(name="b1")
+
+
+
+@RequestScoped
+public class QuoteRequest {
+private Date quoteDate = new Date();
+private String sym = "MSFT";
+public QuoteRequest() {
+System.out.println("Creating b1");
+}
+
+public String getSym() {
+System.out.println("getting sym");
+return sym;
+}
+public void setSym(String sym) {
+System.out.println("setting sym to: " + sym);
+this.sym = sym;
+}
+
+public Date getQuoteDate() {
+return quoteDate;
+}
+public void setQuoteDate(Date quoteDate) {
+this.quoteDate = quoteDate;
+}
+public int getStockValue() {
+return Math.abs((sym+quoteDate.toString()).hashCode());
+}
+}
---
+++ src/stock/StockService.java
@@ -0,0 +1,26 @@
+package stock;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.model.SelectItem;
+
+
+@ManagedBean(name="b2")
+@RequestScoped
+public class StockService {
+private List<SelectItem> symbols;
+public StockService() {
+symbols = new ArrayList<SelectItem>();
+symbols.add(new SelectItem("MSFT"));
+symbols.add(new SelectItem("IBM"));
+symbols.add(new SelectItem("RHAT"));
+}
+public List<SelectItem> getSymbols() {
+return symbols;
+}
+public int getStockValue(QuoteRequest r) {
+return Math.abs((r.getSym() + r.getQuoteDate().toString()).hashCode());
+}
+}
